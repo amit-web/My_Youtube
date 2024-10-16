c393be61128f0c02ac43fde6756bb73a4f7fdece
amit-web <mehtaamit9334@gmail.com>
2024-10-16 21:56:22 +0530

    updated google api key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ const GOOGLE_API_KEY = "AIzaSyAcrwbZOkTMOF4lrlcuinf63Q334yHA9e0"
 
 export const MOST_POPULAR_VIDEO_URL = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + GOOGLE_API_KEY;
 
-export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const YOUTUBE_SEARCH_API = `http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&${GOOGLE_API_KEY}&q=`
 
 export const  YOUTUBE_SEARCH_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&regionCode=IN&q="
 
